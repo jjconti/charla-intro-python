1049b8de5a31321b87c1da1229bd8e20a059229d
Juanjo Conti <jjconti@gmail.com>
2016-06-14 01:32:12 -0300

    Fix

--- ejercicios/watermark/watermark.py
+++ ejercicios/watermark/watermark.py
@@ -4,6 +4,7 @@ import glob
 import os
 from PIL import Image, ImageDraw, ImageFont
 
+
 def main(origins='pics/*', destination='wpics'):
     for i in glob.glob(origins):
         watermark(i, destination)
@@ -14,7 +15,7 @@ def watermark(image_file, destination):
     watermark = Image.new("RGBA", image.size)
     waterdraw = ImageDraw.ImageDraw(watermark, "RGBA")
 
-    waterdraw.text((10, 10), "juanjoconti.com", font=ImageFont.truetype("open-sans/OpenSans-Bold.ttf", 38))
+    waterdraw.text((20, 10), "juanjoconti.com", font=ImageFont.truetype("open-sans/OpenSans-Bold.ttf", 38))
 
     watermask = watermark.convert("L").point(lambda x: min(x, 100))
 
